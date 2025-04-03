176bb19797c1e7265492fc9f6349eeadff2dc315
Savecoders <pablopincay2003@gmail.com>
2025-04-03 15:53:50 -0500

    feat: Tailwind conf and  markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import { rehypeHeadingIds } from '@astrojs/markdown-remark';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark-default',
+    },
+    rehypePlugins: [rehypeHeadingIds],
+  },
+  experimental: {
+    responsiveImages: true,
+    svg: true,
+  },
+});
