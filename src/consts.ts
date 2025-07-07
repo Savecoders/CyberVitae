57dd7878a5f3de843e825e262ca0b0efae9fdabc
Savecoders <pablopincay2003@gmail.com>
2025-07-06 21:23:53 -0500

    feat: content collections and utitlis

---
+++ src/consts.ts
@@ -0,0 +1,51 @@
+import type { IconMap, SocialLink, Site } from '@/types'
+
+export const SITE: Site = {
+  title: 'CyberVitae',
+  description:
+    'Pagina donde encontraras blogs sobre Linux e informática en general, con un enfoque en la terminal y la programación.',
+  href: 'https://astro-erudite.vercel.app',
+  author: 'Savecoders',
+  locale: 'en-US',
+  featuredPostCount: 2,
+  postsPerPage: 3,
+}
+
+export const NAV_LINKS: SocialLink[] = [
+  {
+    href: '/blog',
+    label: 'blog',
+  },
+  {
+    href: '/authors',
+    label: 'authors',
+  },
+]
+
+export const SOCIAL_LINKS: SocialLink[] = [
+  {
+    href: 'https://github.com/jktrn',
+    label: 'GitHub',
+  },
+  {
+    href: 'https://twitter.com/enscry',
+    label: 'Twitter',
+  },
+  {
+    href: 'mailto:jason@enscribe.dev',
+    label: 'Email',
+  },
+  {
+    href: '/rss.xml',
+    label: 'RSS',
+  },
+]
+
+export const ICON_MAP: IconMap = {
+  Website: 'lucide:globe',
+  GitHub: 'lucide:github',
+  LinkedIn: 'lucide:linkedin',
+  Twitter: 'lucide:twitter',
+  Email: 'lucide:mail',
+  RSS: 'lucide:rss',
+}
